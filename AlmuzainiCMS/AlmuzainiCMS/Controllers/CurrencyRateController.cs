71ff9be66e6003f2800674737b5181f97245c501
imran <imran_bof@live.com>
2023-08-03 11:06:41 +0600

    currency button

---
+++ AlmuzainiCMS/AlmuzainiCMS/Controllers/CurrencyRateController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmuzainiCMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyRateController : ControllerBase
+    {
+    }
+}
