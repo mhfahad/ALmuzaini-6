1c54fcf6eba51ed9ab4adafc331085a78646ea27
Julkar Nime <julkarnime09@gmail.com>
2023-08-08 02:33:36 +0600

    Upload Top Slider,Rate Calculator,Middle Slider,Round Button,Last Slider , Video
    Upload single and multiple Top Slider,Rate Calculator,Middle Slider,Round Button,Last Slider , Video file to specific location folder wise  and create thumnail of file and store thumnail in thumnail folder

--- AlmuzainiCMS/AlmuzainiCMS/Controllers/HomeController.cs
+++ AlmuzainiCMS/AlmuzainiCMS/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using AlmuzainiCMS.Models;
+﻿using AlmuzainiCMS.BLL.Interface;
+using AlmuzainiCMS.Models;
+using AlmuzainiCMS.Models.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing.Internal;
@@ -11,10 +15,14 @@ namespace AlmuzainiCMS.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+       
+        private readonly IMapper _mapper;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public HomeController(ILogger<HomeController> logger ,  IMapper mapper, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _mapper = mapper;
+            _hostingEnvironment = webHostEnvironment;
         }
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
@@ -42,34 +50,227 @@ namespace AlmuzainiCMS.Controllers
 
             return View();
         }
-        public async Task<JsonResult> UploadTopSlider([FromForm] FileUpload  fileUpload)
+        [HttpPost]
+        public IActionResult UploadTopSlider(MultipleFileUploadVM model)
         {
-         
-
-            try
+            foreach (var file in model.Files)
             {
-                foreach (var formFile in fileUpload.ImgFiles)
+                if (file != null && file.Length > 0)
                 {
-                    if (formFile.Length > 0)
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploadsFolder, "original", "TopSlider", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
                     {
-                        var fileName = formFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
-                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "~/Uploads/Home/", fileName);
+                        file.CopyTo(fileStream);
+                    }
 
-                        using (var stream = System.IO.File.Create(filePath))
+                    //Generate a thumbnail and save it
+                    using (var imageStream = file.OpenReadStream())
+                    using (var image = Image.Load(imageStream))
+                    {
+                        var thumbnail = image.Clone(x => x.Resize(new ResizeOptions
                         {
-                            await formFile.CopyToAsync(stream);
-                        }
+                            Size = new Size(300, 300),
+                            Mode = ResizeMode.Max
+                        }));
 
-                        return Json(true);
+                        string thumbnailPath = Path.Combine(uploadsFolder, "thumbnails", "TopSlider");
+                        int totalfiles = Directory.GetFiles(thumbnailPath).Count();
+
+                        string thumbnailPathWithCount = Path.Combine(thumbnailPath, (totalfiles + 1).ToString() + fileExtension);
+                        thumbnail.Save(thumbnailPathWithCount);
                     }
                 }
             }
-            catch (Exception exception)
-            {
-                return Json(false);
-            }
-            return Json(false);
+
+            return RedirectToAction("Index", "Home");
         }
+
+        [HttpPost]
+        public IActionResult UploadRateCalculator(MultipleFileUploadVM model)
+        {
+            foreach (var file in model.Files)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploadsFolder, "original", "RateCalculator", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    //Generate a thumbnail and save it
+                    using (var imageStream = file.OpenReadStream())
+                    using (var image = Image.Load(imageStream))
+                    {
+                        var thumbnail = image.Clone(x => x.Resize(new ResizeOptions
+                        {
+                            Size = new Size(300, 300),
+                            Mode = ResizeMode.Max
+                        }));
+
+                        string thumbnailPath = Path.Combine(uploadsFolder, "thumbnails", "RateCalculator");
+                        int totalfiles = Directory.GetFiles(thumbnailPath).Count();
+
+                        string thumbnailPathWithCount = Path.Combine(thumbnailPath, (totalfiles + 1).ToString() + fileExtension);
+                        thumbnail.Save(thumbnailPathWithCount);
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadMiddleSlider(MultipleFileUploadVM model)
+        {
+            foreach (var file in model.Files)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploadsFolder, "original", "MiddleSlider", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    //Generate a thumbnail and save it
+                    using (var imageStream = file.OpenReadStream())
+                    using (var image = Image.Load(imageStream))
+                    {
+                        var thumbnail = image.Clone(x => x.Resize(new ResizeOptions
+                        {
+                            Size = new Size(300, 300),
+                            Mode = ResizeMode.Max
+                        }));
+
+                        string thumbnailPath = Path.Combine(uploadsFolder, "thumbnails", "MiddleSlider");
+                        int totalfiles = Directory.GetFiles(thumbnailPath).Count();
+
+                        string thumbnailPathWithCount = Path.Combine(thumbnailPath, (totalfiles + 1).ToString() + fileExtension);
+                        thumbnail.Save(thumbnailPathWithCount);
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadRoundButtons(MultipleFileUploadVM model)
+        {
+            foreach (var file in model.Files)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploadsFolder, "original", "RoundButtons", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    //Generate a thumbnail and save it
+                    using (var imageStream = file.OpenReadStream())
+                    using (var image = Image.Load(imageStream))
+                    {
+                        var thumbnail = image.Clone(x => x.Resize(new ResizeOptions
+                        {
+                            Size = new Size(300, 300),
+                            Mode = ResizeMode.Max
+                        }));
+
+                        string thumbnailPath = Path.Combine(uploadsFolder, "thumbnails", "RoundButtons");
+                        int totalfiles = Directory.GetFiles(thumbnailPath).Count();
+
+                        string thumbnailPathWithCount = Path.Combine(thumbnailPath, (totalfiles + 1).ToString() + fileExtension);
+                        thumbnail.Save(thumbnailPathWithCount);
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult UploadLastSlider(MultipleFileUploadVM model)
+        {
+            foreach (var file in model.Files)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploadsFolder, "original", "LastSlider", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    //Generate a thumbnail and save it
+                    using (var imageStream = file.OpenReadStream())
+                    using (var image = Image.Load(imageStream))
+                    {
+                        var thumbnail = image.Clone(x => x.Resize(new ResizeOptions
+                        {
+                            Size = new Size(300, 300),
+                            Mode = ResizeMode.Max
+                        }));
+
+                        string thumbnailPath = Path.Combine(uploadsFolder, "thumbnails", "LastSlider");
+                        int totalfiles = Directory.GetFiles(thumbnailPath).Count();
+
+                        string thumbnailPathWithCount = Path.Combine(thumbnailPath, (totalfiles + 1).ToString() + fileExtension);
+                        thumbnail.Save(thumbnailPathWithCount);
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult UploadVideo(MultipleFileUploadVM model)
+        {
+            foreach (var file in model.Files)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                    string fileExtension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploadsFolder, "original", "Videos", uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
