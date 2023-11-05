62fa97604e4014718225b550e88ff85960f36de2
Mostafa Mahatabe <mahatabe.omi@gmail.com>
2023-11-05 21:26:47 +0600

    omi

--- AlmuzainiCMS/AlmuzainiCMS.Models/Models/Branches.cs
+++ AlmuzainiCMS/AlmuzainiCMS.Models/Models/Branches.cs
@@ -35,9 +35,12 @@ namespace AlmuzainiCMS.Models.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
 
+        public string? SelectedDropdownValue { get; set; }
         public string? Area { get; set; }
         public string? Adress { get; set; }
         public string? Time { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
         public string? Map { get; set; }
 
         public DateTime? CreatedAt { get; set; }
